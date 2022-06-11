6e6ce400fd1eb38817fcad9ab809b2349dafdbf5
kyrlm <kyriaki.lm@gmail.com>
2022-06-11 19:42:43 +0300

    new code

--- final code/VaultMe/src/History.java
+++ final code/VaultMe/src/History.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 
-enum HistoryGroups{Outgoing, Ingoing, All};
+enum HistoryGroups{Outgoing, Ingoing, All}
 public class History {
 
-    public static ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+    public static ArrayList<Transaction> transactions = new ArrayList<>();
 
 
     public static String showHistory(HistoryGroups type){
@@ -11,13 +11,12 @@ public class History {
         String out = "*** Transaction List ***\n";
         for(Transaction transaction : History.transactions) {
             if (application.loggedInUser.getWallet().bankAccounts.contains(transaction.getFrom()) && (type==HistoryGroups.Outgoing || type==HistoryGroups.All) )
-                out = out.concat(" Outgoing Transaction: " + " To: " + transaction.getTo() + " Amount: " + transaction.getAmount() +
+                out = out.concat(" Outgoing Transaction: " + " To: " + transaction.getTo().toString() + " Amount: " + transaction.getAmount() +
                         " Transaction Id: " + transaction.getId() + " Type: " + transaction.getType() + "\n");
 
             if (application.loggedInUser.getWallet().bankAccounts.contains(transaction.getTo()) && (type==HistoryGroups.Ingoing || type==HistoryGroups.All))
                 out = out.concat(" Ingoing Transaction: " + " From: " + transaction.getFrom().toString() +
                         " Amount: " + transaction.getAmount() + " Transaction Id: " + transaction.getId() + " Type: " + transaction.getType() + "\n");
-
         }
         return out;
     }
